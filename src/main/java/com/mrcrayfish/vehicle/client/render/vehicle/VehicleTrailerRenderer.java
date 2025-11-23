4833e34efe0c0133adb0104f1a3abea58a09f9b9
miyamashak <basebollmonkey@gmail.com>
2025-11-23 19:21:07 +0300

    Revert "Continue to work on wheel sizes"
    This reverts commit 0dbc267a4d066a241c39d5aef28143c773c7c117.

--- src/main/java/com/mrcrayfish/vehicle/client/render/vehicle/VehicleTrailerRenderer.java
+++ src/main/java/com/mrcrayfish/vehicle/client/render/vehicle/VehicleTrailerRenderer.java
@@ -25,8 +25,8 @@ public class VehicleTrailerRenderer extends AbstractTrailerRenderer<VehicleEntit
     protected void render(@Nullable VehicleEntityTrailer vehicle, PoseStack matrixStack, MultiBufferSource renderTypeBuffer, float partialTicks, int light)
     {
         this.renderDamagedPart(vehicle, SpecialModels.VEHICLE_TRAILER.getModel(), matrixStack, renderTypeBuffer, light);
-        this.renderWheel(vehicle, matrixStack, renderTypeBuffer, false, -14.5F * 0.0625F, -0.45F, -2.5F * 0.0625F, 1.75F, partialTicks, light);
-        this.renderWheel(vehicle, matrixStack, renderTypeBuffer, true, 14.5F * 0.0625F, -0.45F, -2.5F * 0.0625F, 1.75F, partialTicks, light);
+        this.renderWheel(vehicle, matrixStack, renderTypeBuffer, false, -14.5F * 0.0625F, -0.5F, -2.5F * 0.0625F, 1.25F, partialTicks, light);
+        this.renderWheel(vehicle, matrixStack, renderTypeBuffer, true, 14.5F * 0.0625F, -0.5F, -2.5F * 0.0625F, 1.25F, partialTicks, light);
     }
 
     @Nullable
